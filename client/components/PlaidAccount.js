88225a85ae2a376a62799419f698eb7663e1cb49
Katie Agresta <katieagresta1@gmail.com>
2019-05-03 15:33:28 -0400

    added oauth component

--- client/components/PlaidAccount.js
+++ client/components/PlaidAccount.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-
+import { addingAccount } from '../store/accountReducer';
 class PlaidAccount extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {}
   render() {
     return <div />;
   }
--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import userReducer from './userReducer';
+import accountReducer from './accountReducer';
 
-const reducer = combineReducers({ userReducer });
+const reducer = combineReducers({ userReducer, accountReducer });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
