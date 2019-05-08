a9ef3afd5d418a1c2574f9cbaac7259ddbb42cc8
Charlyn Manuyag <charlyn.manuyag@berkeley.edu>
2019-05-08 12:50:28 -0400

    got our array of balance for checking and savings

--- client/components/dashboard/Summary.js
+++ client/components/dashboard/Summary.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { gettingAccounts } from '../../store/accountReducer';
-import SummaryContainer from './SummaryContainer';
-import axios from 'axios';
+import { gettingAccounts, gettingBalance } from '../../store/accountReducer';
 
 class Summary extends React.Component {
   constructor(props) {
@@ -15,6 +13,7 @@ class Summary extends React.Component {
   async componentDidMount() {
     await this.props.gettingAccounts();
     if (this.props.accounts.length) {
+      await this.props.gettingBalance(this.props.accounts);
       this.setState({ loading: true, accountsExist: true });
     } else {
       this.setState({ loading: true });
@@ -22,13 +21,25 @@ class Summary extends React.Component {
   }
   render() {
     const accounts = this.props.accounts;
-    console.log(this.props.accounts, 'ACCOUNTS IN SUMMARY');
+    // console.log(this.props.balance, 'BALANCE IN SUMMARY');
+    let temp = [];
+    let types = ['Plaid Checking', 'Plaid Saving'];
+    this.props.balance.forEach(function(element) {
+      element.balance.forEach(function(ele) {
+        temp.push({
+          AccountName: element.accountName,
+          AvailableBalance: ele.balances.available,
+          CurrentBalance: ele.balances.current,
+          Type: ele.name,
+        });
+      });
+    });
+    // balanceData array includes only Savings and Checkings
+    const balanceData = temp.filter(data => types.includes(data.Type));
     return (
       <div>
         {this.state.loading && this.state.accountsExist ? (
-          <div>
-            <SummaryContainer accounts={accounts} />
-          </div>
+          <div />
         ) : !this.loading ? (
           <h1>Loading</h1>
         ) : (
@@ -41,10 +52,13 @@ class Summary extends React.Component {
 
 const mapState = state => ({
   accounts: state.accountReducer.accounts,
+  balance: state.accountReducer.balance,
 });
 
 const mapDispatch = dispatch => ({
   gettingAccounts: () => dispatch(gettingAccounts()),
+  gettingBalance: plaidAccountData =>
+    dispatch(gettingBalance(plaidAccountData)),
 });
 
 export default connect(
