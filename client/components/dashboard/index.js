cc79084c8b183f769266f770aab7817c5f1d0780
Ramya Sampath <ramyasampath2002@yahoo.com>
2019-05-13 15:10:34 -0400

    updated refresh on delete

--- client/components/dashboard/index.js
+++ client/components/dashboard/index.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import Summary from './Summary';
-import ConnectedPlaidAccount from '../PlaidAccount';
-import PlaidAccountTransactions from '../dashboard/Transaction';
-import InsightCard from './insights/InsightCards';
-import { connect } from 'react-redux';
-import CategoriesDonut from './CategoriesDonutChart';
-import Navbar from '../Navbar';
+import React, { Component } from "react";
+import Summary from "./Summary";
+import ConnectedPlaidAccount from "../PlaidAccount";
+import PlaidAccountTransactions from "../dashboard/Transaction";
+import InsightCard from "./insights/InsightCards";
+import { connect } from "react-redux";
+import CategoriesDonut from "./CategoriesDonutChart";
+import Navbar from "../Navbar";
 import {
   gettingAccounts,
   gettingTransactions,
-  gettingBalance,
-} from '../../store/accountReducer';
+  gettingBalance
+} from "../../store/accountReducer";
 import {
   getLargest,
   getRestaurantSpend,
   getMerchantSpend,
   getTranspoSpend,
-  getFees,
-} from '../../store/insightReducer';
+  getFees
+} from "../../store/insightReducer";
 
-import Loading from './Loading';
+import Loading from "./Loading";
 
-import { logoutUser } from '../../store/userReducer';
-import '../dashboard/Summary.css';
+import { logoutUser } from "../../store/userReducer";
+import "../dashboard/Summary.css";
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: false,
+      loading: false
     };
   }
   async componentDidMount() {
@@ -86,7 +86,7 @@ class Dashboard extends Component {
 const mapStateToProps = state => ({
   user: state.userReducer,
   accounts: state.accountReducer.accounts,
-  transactions: state.accountReducer.transactions,
+  transactions: state.accountReducer.transactions
 });
 const mapDispatchToProps = dispatch => ({
   gettingTransactions: plaidAccountData =>
@@ -99,7 +99,7 @@ const mapDispatchToProps = dispatch => ({
   getRestaurantSpend: props => dispatch(getRestaurantSpend(props)),
   getMerchantSpend: props => dispatch(getMerchantSpend(props)),
   getTranspoSpend: props => dispatch(getTranspoSpend(props)),
-  getFees: props => dispatch(getFees(props)),
+  getFees: props => dispatch(getFees(props))
 });
 export default connect(
   mapStateToProps,
