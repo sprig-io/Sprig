1db7151130d41b20a5c2ba283493c289c8972506
Ramya Sampath <ramyasampath2002@yahoo.com>
2019-05-14 14:31:00 -0400

    new table

--- client/components/dashboard/Transaction.js
+++ client/components/dashboard/Transaction.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import ReactTable from "material-table";
-//import * as startOfDay from 'date-fns';
+import ReactDOM from "react-dom";
+import MUIDataTable from "mui-datatables";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 
 class Transactions extends Component {
   constructor(props) {
@@ -17,38 +18,49 @@ class Transactions extends Component {
     this.props.logoutUser();
   }
 
+  getMuiTheme = () =>
+    createMuiTheme({
+      overrides: {
+        MUIDataTableBodyCell: {
+          root: {
+            backgroundColor: "white",
+            maxwidth: "50%",
+            padding: "8px",
+            leftmargin: "50px",
+            rightmargin: "50px"
+          }
+        }
+      }
+    });
   render() {
     let transData = [];
     this.props.transactions.forEach(function(element) {
       transData.push({
-        Name: element.accountName,
+        Date: new Date(element.date).toDateString(),
+        Category: element.category[0],
+        Vendor: element.name,
         Amount: element.amount,
-        category: element.category[0],
-        companyName: element.name,
-        date: new Date(element.date).toDateString()
+        Bank: element.accountName
       });
     });
 
-    let columnNames = [
-      { title: "Date", field: "date" },
-      { title: "Category", field: "category" },
-      { title: "Vendor", field: "companyName" },
-      { title: "Amount", field: "Amount" },
-      { title: "Bank Name", field: "Name" }
-    ];
+    const columns = ["Date", "Category", "Vendor", "Amount", "Bank"];
+
     return (
-      <div
-        style={{
-          marginRight: "25px",
-          marginLeft: "25px"
-        }}
-        className="tabletrans"
-      >
-        <ReactTable
-          title="Transactions"
-          data={transData}
-          columns={columnNames}
-        />
+      <div className="containerTable">
+        <MuiThemeProvider theme={this.getMuiTheme()}>
+          <MUIDataTable
+            style={{ width: "90%" }}
+            title={"Transactions"}
+            data={transData}
+            columns={columns}
+            options={{
+              selectableRows: false,
+              filter: false,
+              viewColumns: false
+            }}
+          />
+        </MuiThemeProvider>
       </div>
     );
   }
