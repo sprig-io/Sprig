a474c947e64082db5308cbafe1596a3fae719a8b
lucymar <lucygibb68@gmail.com>
2019-05-15 11:04:50 -0400

    config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 (process.env.mongoURI =
-  'mongodb+srv://groupSprig:groupSprig@capstone-hw9wn.mongodb.net/test?retryWrites=true'),
-  (process.env.secretOrKey = '96b52e49-8ee0-4c68-aee3-31a44b97b9ac'),
-  (process.env.tokenLife = 900),
-  (process.env.refreshTokenLife = 86400);
+  'mongodb+srv://groupSprig:groupSprig@capstone-hw9wn.mongodb.net/test?retryWrites=true')(
+  (process.env.secretOrKey = '96b52e49-8ee0-4c68-aee3-31a44b97b9ac')
+)((process.env.tokenLife = 900))((process.env.refreshTokenLife = 86400));
